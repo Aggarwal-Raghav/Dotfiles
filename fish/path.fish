ecff3a641f632fa72990da92ff38cd2fd8ec14b8
Raghav Aggarwal <raghavaggarwal03.ra@gmail.com>
2024-09-16 20:50:38 +0530

    Sync fish

--- fish/path.fish
+++ fish/path.fish
@@ -12,20 +12,22 @@ set -x PATH $(brew --prefix)/opt/python@3.12/libexec/bin $PATH
 # java17 path #
 ###############
 set -x JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8/Contents/Home
+# set -x JAVA_HOME /Library/Java/JavaVirtualMachines/openjdk-17.jdk/Contents/Home/
 set -x PATH $JAVA_HOME/bin $PATH
 
 #########
 # maven #
 #########
 set -x MAVEN_OPTS "-Dspring.profiles.active=local"
-set -x M2_HOME $(brew --prefix)/Cellar/maven/3.9.6/libexec
+set -x M2_HOME $(brew --prefix)/Cellar/maven/3.9.9/libexec
 set -x M2_REPO $HOME/.m2/repository
 
 ##########
 # gradle #
 ##########
-set -x GRADLE_HOME $(brew --prefix)/Cellar/gradle/8.5/libexec
+set -x GRADLE_HOME /Users/raghav/Desktop/software/gradle
 set -x GRADLE_OPTS "-Dgradle.user.home=$XDG_CACHE_HOME/gradle"
+set -x PATH $GRADLE_HOME/bin $PATH
 
 #####################
 # sublime text path #
@@ -67,5 +69,12 @@ set -x GNUPGHOME /Users/raghav/.config/gpg
 ##########
 # protoc #
 ##########
-set -l PROTOC $HOME/Desktop/personal/software/protoc
+set -l PROTOC $HOME/Desktop/software/protoc
 set -x PATH $PATH $PROTOC/bin
+
+
+#########################
+# General ENV Variables #
+#########################
+set -x VISUAL nvim
+set -x EDITOR nvim
